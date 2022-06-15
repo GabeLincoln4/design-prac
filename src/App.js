8dc0fe54f78532efabe9c0a8ab614f178288a939
Gavin <7797864896@K12.LCS>
2022-06-15 16:00:34 -0400

    Add contact component with data accordingly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './components/Hero.jsx';
 import Navbar from './components/Navbar.jsx';
 import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Contact />
     </div>
     
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Section</h1>
+            <h2>Contact Us!</h2>
+            <div>
+                <h3>Phone Number</h3>
+                <p>433-333-3333</p>
+                <h3>Email Address</h3>
+                <p>address@email.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
